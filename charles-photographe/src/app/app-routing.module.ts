4ce2b42dd2b0bd6f80e8f5bd113563f681b1c758
Abel Ciccoli <abelcoli@outlook.fr>
2023-02-28 16:10:06 +0100

    feat(project): Added angular filestructure to project

---
+++ charles-photographe/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {RouterModule, Routes} from "@angular/router";
+import {ReactiveFormsModule} from "@angular/forms";
+
+const routes:Routes = [];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    RouterModule.forRoot(routes),
+    ReactiveFormsModule
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
