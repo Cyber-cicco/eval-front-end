ad1abdd40875efe38430f3874113e021057ddbc4
Abel Ciccoli <abelcoli@outlook.fr>
2023-03-01 15:13:13 +0100

    feat(project): Added angular filestructure to project

--- charles-photographe/src/app/app.module.ts
+++ charles-photographe/src/app/app.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AccueilComponent } from './pages/accueil/accueil.component';
+import { GalerieComponent } from './pages/galerie/galerie.component';
+import { TarifsComponent } from './pages/tarifs/tarifs.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    GalerieComponent,
+    TarifsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ charles-photographe/src/app/pages/galerie/galerie.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cp-galerie',
+  templateUrl: './galerie.component.html',
+  styleUrls: ['./galerie.component.scss']
+})
+export class GalerieComponent {
+
+}
