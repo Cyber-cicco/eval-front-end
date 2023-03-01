718c9146f464132d0f303977bf9a3d439ce87653
Abel Ciccoli <abelcoli@outlook.fr>
2023-03-01 18:54:15 +0100

    feat(galerie): Added bugs in galerie

--- charles-photographe/src/app/pages/galerie/galerie.component.ts
+++ charles-photographe/src/app/pages/galerie/galerie.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgIf } from '@angular/common';
+import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'cp-galerie',
+  styleUrls: ['galerie.component.scss'],
   templateUrl: './galerie.component.html',
-  styleUrls: ['./galerie.component.scss']
 })
 export class GalerieComponent {
-
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
 }
